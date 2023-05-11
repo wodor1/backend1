1cccb5872b91a771cbff9a91b1c049a289dd5897
Csikvár Tamás <csikvar.tamas@gmail.com>
2023-05-11 13:41:49 +0200

    second

---
+++ nested_loops.py
@@ -0,0 +1,175 @@
+# első feladat
+
+m = [[1, 1, 1, 2, 1], [2, 3, 2, 2, 2], [3, 3, 2, 3, 3], [4, 4, 4, 3, 4]]
+
+
+def matrix_sum(matrix):
+    summa = 0
+    for row in matrix:
+        for element in row:
+            summa += element
+    return summa
+
+
+print(matrix_sum(m))
+
+# második feladat
+
+cars = [
+    {"price": 1549,
+     "passengerQty": 4,
+     "currency": "EUR",
+     "type": "Kia",
+     "transmission": "manual",
+     "passengers": [
+         "Gabor",
+         "Andras",
+         "Timea",
+         "Martin",
+         "Miklos"
+     ]
+     },
+    {"price": 1954,
+     "passengerQty": 5,
+     "currency": "EUR",
+     "type": "Suzuki",
+     "transmission": "manual",
+     "passengers": [
+         "Gabor",
+         "Andras",
+         "Timea",
+         "Martin",
+         "Miklos"
+     ]
+     },
+    {"price": 2544,
+     "passengerQty": 5,
+     "currency": "USD",
+     "type": "Opel",
+     "transmission": "manual",
+     "passengers": [
+         "Gabor",
+         "Andras",
+         "Timea",
+         "Martin",
+         "Miklos"
+     ]
+     },
+    {"price": 2544,
+     "passengerQty": 2,
+     "currency": "USD",
+     "type": "Opel",
+     "transmission": "manual",
+     "passengers": [
+         "Gabor",
+         "Timea",
+         "Miklos"
+     ]
+     },
+    {"price": 9542,
+     "passengerQty": 4,
+     "currency": "USD",
+     "type": "Ford",
+     "transmission": "automatic",
+     "passengers": [
+         "Gabor",
+         "Timea",
+         "Miklos"
+     ]
+     },
+]
+
+for car in cars:
+    if len(car["passengers"]) > car["passengerQty"]:
+        print("A(z) {} típusú autóban túl sok az utas".format(car["type"]))
+
+# harmadik feladat
+
+cars = [
+    {"price": 1549,
+     "passengerQty": 4,
+     "currency": "EUR",
+     "type": "Kia",
+     "transmission": "manual",
+     "passengers": [
+         "Gabor",
+         "Andras",
+         "Timea",
+         "Martin",
+         "Miklos"
+     ]
+     },
+    {"price": 1954,
+     "passengerQty": 5,
+     "currency": "EUR",
+     "type": "Suzuki",
+     "transmission": "manual",
+     "passengers": [
+         "Gabor",
+         "Andras",
+         "Timea",
+         "Martin",
+         "Miklos"
+     ]
+     },
+    {"price": 2544,
+     "passengerQty": 5,
+     "currency": "USD",
+     "type": "Opel",
+     "transmission": "manual",
+     "passengers": [
+         "Gabor",
+         "Andras",
+         "Timea",
+         "Martin",
+         "Miklos"
+     ]
+     },
+    {"price": 2544,
+     "passengerQty": 2,
+     "currency": "USD",
+     "type": "Opel",
+     "transmission": "manual",
+     "passengers": [
+         "Gabor",
+         "Timea",
+         "Miklos"
+     ]
+     },
+    {"price": 9542,
+     "passengerQty": 4,
+     "currency": "USD",
+     "type": "Ford",
+     "transmission": "automatic",
+     "passengers": [
+         "Gabor",
+         "Timea",
+         "Miklos"
+     ]
+     },
+]
+
+for car in cars:
+    if car["currency"] == "EUR":
+        car["price"] = car["price"] * 1.1
+        car["currency"] = "USD"
+    print("A(z) {} típusú autó ára: {} {}".format(
+        car["type"], car["price"], car["currency"]))
+
+# negyedik feladat
+
+persons = ["John", "Marissa", "Pete", "Dayton"]
+restaurants = ["Japanese", "American", "Mexican", "French"]
+
+for person in persons:
+    for restaurant in restaurants:
+        print("{} eats {}".format(person, restaurant))
+
+# ötödik feladat
+
+my_list = [[4, 5], [7, 4], [2, 5], [9, 4]]
+
+for sublist in my_list:
+    sublist.append(sum(sublist))
+
+print(my_list)
--- python_fgv.py
+++ python_fgv.py
@@ -1,3 +1,9 @@
+# első feladat
+
+
+import math
+
+
 def find_numbers(start=2000, finish=3200, oszthato=7, nem_oszthato=5):
     result = []
 
@@ -10,3 +16,67 @@ def find_numbers(start=2000, finish=3200, oszthato=7, nem_oszthato=5):
 
 result = find_numbers()
 print(result)
+
+# második feladat
+
+
+def count_pista(names_list):
+    return names_list.count("Pista")
+
+
+names_list = ['Pista', 'Gabor', 'Pista', 'Gábor', 'István', 'István',
+              'László', 'Katalin', 'Katalin', 'Tímea', 'Gábor', 'Pista']
+pista_count = count_pista(names_list)
+print(pista_count)
+
+# harmadik feladat
+
+
+def convert_to_number(text):
+    try:
+        number = int(text)
+        return number
+    except ValueError:
+        print("A megadott érték nem konvertálható egész számmá.")
+        return None
+
+
+def print_hello(count):
+    for i in range(1, count + 1):
+        print(f"{i}. Helló!")
+
+
+user_input = input("Kérlek, adj meg egy egész számot: ")
+converted_number = convert_to_number(user_input)
+
+if converted_number is not None:
+    print_hello(converted_number)
+
+# negyedik feladat
+
+
+def szamra_alakit(szoveg):
+    try:
+        szam = int(szoveg)
+        return szam
+    except ValueError:
+        print("A megadott érték nem alakítható át egész számmá.")
+        return None
+
+
+def kor_terulet(sugar):
+    return math.pi * sugar ** 2
+
+
+def kor_kerulete(sugar):
+    return 2 * math.pi * sugar
+
+
+bevitel = input("Kérlek, add meg a kör sugarának egész számát: ")
+sugar = szamra_alakit(bevitel)
+
+if sugar is not None:
+    terulet = kor_terulet(sugar)
+    kerulet = kor_kerulete(sugar)
+    print(f"A kör területe: {terulet}")
+    print(f"A kör kerülete: {kerulet}")
